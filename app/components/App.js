674c8c5b61035fd379ffd46ebccdcf49b6652db8
Gregory D Stula <gregstula.dev@icloud.com>
2017-05-02 16:07:41 -0400

    Add navbar

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Popular from './popular';
+import { 
+  BrowserRouter as Router,
+  Route      
+} from 'react-router-dom';
+import Nav from './nav';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Route path='/popular' component={Popular}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ app/components/nav.js
@@ -0,0 +1,32 @@
+/*
+ * nav.js
+ * Copyright (C) 2017 g <g@FreeBSD64>
+ *
+ * Distributed under terms of the MIT license.
+ */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <ul className='nav'>
+      <li>
+        <NavLink exact activeClassName='active' to='/'>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/battle'>
+          Battle
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/popular'>
+          Popular
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
